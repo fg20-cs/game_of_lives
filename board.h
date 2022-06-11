2e5159277bd6be4cc177570a0f884a3a35cd6496
Fidan Guliyeva <f2guliyeva@gmail.com>
2022-06-11 16:46:34 +0400

    added some structs for board and view

--- board.h
+++ board.h
@@ -1,4 +1,48 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+#define FALSE 0
+#define TRUE 1
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <time.h>
+#include <math.h>
+#include <ctype.h>
+
+#include "SDL2/SDL.h"
+
+//Living cells
+#define LIVING_CELL_R 255
+#define LIVING_CELL_G 255
+#define LIVING_CELL_B 255
+
+//Dead cells
+#define DEAD_CELL_R 15
+#define DEAD_CELL_G 15
+#define DEAD_CELL_B 15
+
+typedef Uint8 boole;
+
+typedef struct{
+    int rows, columns;
+    boole grid[0];
+} board;
+
+typedef struct{
+
+    int viewX,
+        viewY,
+        cellSize,
+        cellHeight,
+        cellWidth,
+        windowHeight,
+        windowWidth;
+        
+}view;
+
+
 #endif

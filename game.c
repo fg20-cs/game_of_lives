8f5223d92dd415e5d59a992e3684191247f7050f
Fidan Guliyeva <f2guliyeva@gmail.com>
2022-06-11 23:31:14 +0400

    wrote the structure of the game in main function

--- game.c
+++ game.c
@@ -25,6 +25,51 @@ int main(int argc, char** argv){
         goto RendererCreationError;
     }
 
+
+// Initialize the board and the players view 
+    view play_view;
+    int window_W, window_H;
+    SDL_GL_GetDrawableSize(window, &window_W, &window_H);
+
+    play_view.cellSize = 10;
+    play_view.cellHeight = window_H/play_view.cellSize;
+    play_view.cellWidth = window_W/play_view.cellSize;
+    play_view.windowHeight = window_H;
+    play_view.windowWidth = window_W;
+    play_view.cellSpeed = 3;
+    play_view.minPixelsSpeed = play_view.minPixelsSpeed * play_view.cellSize;
+
+    const int BOARD_Height = play_view.windowHeight/4;
+    const int BOARD_Width = play_view.windowWidth/4;
+    board* cell_board = board_init(BOARD_Height, BOARD_Width, POPULATION_INITIAL);
+
+    play_view.viewX = (BOARD_Width - play_view.cellWidth)/2;
+    play_view.viewY = (BOARD_Height - play_view.cellHeight)/2;
+
+    int living_cells = 0;
+    SDL_Event e;
+    integ last_update_time = 0;
+    integ ticks_per_lifecycle = 1000;
+
+    uint8_t quit = FALSE;
+    uint8_t paused = FALSE;
+
+    draw_board(cell_board, &play_view, renderer);
+
+    // Game loop
+    while (!quit){
+    
+        // Only update the board at the interval stored in ticks_per_lifecycle
+        if (!((SDL_GetTicks() - last_update_time) < ticks_per_lifecycle) && !paused){
+
+            living_cells = update_board(cell_board);
+            last_update_time = SDL_GetTicks();
+        }
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        if (SDL_RenderClear(renderer ))
+            fprintf(stderr, "ERROR: %s\n", SDL_GetError());
+        draw_board(cell_board, &play_view, renderer);
+    }
     
     //Cleaning up
     SDL_DestroyRenderer(renderer);
